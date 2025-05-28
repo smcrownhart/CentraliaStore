579c8914b1ae379e105571b86bdbdf70ee1e2004
Jesse Harlan <jesse.harlan@centralia.edu>
2025-05-28 11:03:05 -0700

    Added basic view for the role manager

---
+++ CentraliaStore/Models/ViewModels/UserRoleViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CentraliaStore.Areas.Identity;
+using Microsoft.AspNetCore.Identity;
+
+namespace CentraliaStore.Models.ViewModels
+{
+    public class UserRoleViewModel
+    {
+        public ICollection<AppUser> Users { get; set; }
+        public ICollection<IdentityRole> Roles { get; set; }
+        public List<RoleModificationViewModel> Additions { get; set; }
+        public List<RoleModificationViewModel> Subtractions { get; set; }
+    }
+}
