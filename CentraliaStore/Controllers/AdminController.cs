a28329cd2ad56a2a9229e000c3494adca4388350
smcrownhart <103695112+smcrownhart@users.noreply.github.com>
2025-05-31 20:47:04 -0700

    Got it working. When you log in as an admin it will redirect to the admin page and it does work. Just don't change the admin username.
    One thing that did directly come from chatGPT was the AdminSeeder class in data and the snippet of code in the main program file that calls it. This way, if the admin gets's deleted it should theoretically seed one. Just in case....

---
+++ CentraliaStore/Controllers/AdminController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using CentraliaStore.Areas.Identity;
+using System.Threading.Tasks;
+
+namespace CentraliaStore.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class AdminController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public AdminController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var users = _userManager.Users.ToList();
+            return View(users);
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(AppUser model)
+        {
+            var user = await _userManager.FindByIdAsync(model.Id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            user.UserName = model.UserName;
+            user.Email = model.Email;
+
+            await _userManager.UpdateAsync(user);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Delete(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            await _userManager.DeleteAsync(user);
+            return RedirectToAction("Index");
+        }
+    }
+}
